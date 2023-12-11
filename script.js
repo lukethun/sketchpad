de93a9c0df1368cd4be07ba2510988b28d43aaa6
lukethun <lukethun@optusnet.com.au>
2023-12-11 14:13:57 +1000

    Fix eventListeners not being applied to grid squares after generating a new grid

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('button');
+let gridDiv = document.querySelectorAll('.grid');
+
+
+
+
 
 button.addEventListener('click', () => {
-
-    const gridSize = prompt('Please enter a grid size:');
-    generateGrid(gridSize, gridSize);
-    gridDiv.remove();
+    deleteGrid();
+    let gridSize = prompt('Please enter grid size');
+    generateGrid(gridSize);      
+    gridDiv = document.querySelectorAll('.grid'); 
+    gridColour();
 })
 
 
 
-const gridDiv = document.querySelectorAll('.grid');
-gridDiv.forEach(grid => {
-    grid.addEventListener('mouseover', () =>{
-        grid.setAttribute('style', 'background-color: rgb(87, 247, 167);')
+function gridColour(){
+    gridDiv.forEach(grid => {
+        grid.addEventListener('mouseover', () =>{
+            grid.setAttribute('style', 'background-color: rgb(87, 247, 167);')
+        })
     })
-})
+}
 
 
 
-function generateGrid(rows, columns){
-    for(let i = 0; i < rows*columns; i++){
+function generateGrid(size){
+    for(let i = 0; i < size*size; i++){
         const gridDiv = document.createElement('div');
         gridDiv.setAttribute('class', 'grid');
         gridDiv.textContent = '';
         container.appendChild(gridDiv);
     }
 };
+
+function deleteGrid(){
+    let gridDiv = document.querySelectorAll('.grid');
+    gridDiv.forEach(grid => {
+        grid.remove();
+    })
+}
