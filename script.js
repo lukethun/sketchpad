635c381e837b5c092bce510aae0c6676b57b4a12
lukethun <lukethun@optusnet.com.au>
2023-12-11 15:18:56 +1000

    Add JS to randomise the RGB value of each grid square when the eventListener is triggered.
    Add a Clear Grid button to set all grid squares back to white.

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('button');
+const clearButton = document.createElement('button');
+const buttonContainer = document.querySelector('.button-container');
 let gridDiv = document.querySelectorAll('.grid');
-
+let gridSize = '';
 
 
 
 // Delete old grid and generate a new one with specified size
 button.addEventListener('click', () => {
-    let gridSize = prompt('Please enter grid size');
-    if(gridSize >= 100){
-        alert('Please enter a number below 100');
+    gridSize = prompt('Please enter grid size');
+    if(gridSize > 100 || gridSize <= 0){
+        alert('Please enter a number between 1-100');
     } else {
     deleteGrid();
-    document.documentElement.style.setProperty(`--box-sizing`, `calc(var(--page-dimensions) / ${gridSize} - 1px`);
+    document.documentElement.style.setProperty(`--box-sizing`, `calc(var(--page-dimensions) / ${gridSize} - 1px`); // Set individual gridBox size to fit within container dimensions
     generateGrid(gridSize);      
     gridDiv = document.querySelectorAll('.grid'); 
     gridColour();
+    clearColours();
     }
 })
 
 
 
+function clearColours(){
+    clearButton.textContent = 'Clear Grid';
+    buttonContainer.appendChild(clearButton);
 
-
-
-function gridColour(){
-    gridDiv.forEach(grid => {
-        grid.addEventListener('mouseover', () =>{
-            grid.setAttribute('style', 'background-color: rgb(87, 247, 167);')
+    clearButton.addEventListener('click', () => {
+        gridDiv.forEach(grid => {
+            grid.setAttribute('style', 'background-color: white;');
         })
     })
+}
+
+function gridColour(colour){
+    gridDiv.forEach(grid => {
+        grid.addEventListener('mouseover', () =>{
+            
+            grid.setAttribute('style', `background-color: ${getRandomRgb()}`);
+            })
+        })
 }
 
 function generateGrid(size){
@@ -39,7 +51,7 @@ function generateGrid(size){
         gridDiv.textContent = '';
         container.appendChild(gridDiv);
     }
-};
+}
 
 function deleteGrid(){
     let gridDiv = document.querySelectorAll('.grid');
@@ -48,3 +60,10 @@ function deleteGrid(){
     })
 }
 
+function getRandomRgb() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ');';
+}
